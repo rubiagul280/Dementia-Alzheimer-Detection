864f4dddde85a5c02d2ba981bcc03378bba287c8
rubiagul280 <114504200+rubiagul280@users.noreply.github.com>
2023-05-24 13:07:58 +0500

    ..

--- Components/Game.js
+++ Components/Game.js
@@ -1,30 +1,20 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 
-import React, { useState } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   StatusBar,
   TouchableOpacity,
   ImageBackground,
   View,
-  Modal,
   Text,
-  ScrollView,
-  TouchableWithoutFeedback,
-  Image,
 } from 'react-native';
 import { Button } from 'react-native-paper';
 import LinearGradient from 'react-native-linear-gradient';
 import colors from '../assets/colors/Colors';
 
 export default function Game({ navigation }) {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const toggleModal = () => {
-    setModalVisible(!modalVisible);
-  };
-
 
   return (
     <>
@@ -57,58 +47,6 @@ export default function Game({ navigation }) {
               onPress={() => navigation.navigate('Instructions')}>
               Game Instructions
             </Button>
-            <Modal visible={modalVisible} animationType="slide" onRequestClose={toggleModal}>
-              <ScrollView>
-                <View style={styles.content}>
-                  <View style={styles.head}>
-                    <Text style={styles.title}>Game Instructions</Text>
-                    <TouchableWithoutFeedback
-                      onPress={() => setModalVisible(false)}>
-                      <View
-                        style={{
-                          height: 40,
-                          width: 40,
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                        }}>
-                        <Image
-                          source={require('./../assets/images/CloseIcon.png')}
-                          resizeMode="contain"
-                          style={{
-                            height: '40%',
-                            width: '40%',
-                            marginLeft: 180,
-                            marginTop: -5,
-                          }}
-                        />
-                      </View>
-                    </TouchableWithoutFeedback>
-                  </View>
-                  <Text style={styles.heading}>Memory Game Instructions</Text>
-                  <Text style={styles.text}>
-                    1. Gameplay:{'\n'}{'\n'}
-                    - Flip two cards face-up in each turn.{'\n'}
-                    - If the cards match, they remain face-up, and you earn a point. {'\n'}
-                    - If the cards do not match, they are flipped back face-down.{'\n'}
-                    - Remember the positions of the cards as you continue playing.{'\n'}
-                    - Continue flipping pairs of cards until all matches are found.{'\n'}{'\n'}
-                    2. Winning: {'\n'}{'\n'}- The game is won when all pairs of cards are successfully matched. {'\n'}{'\n'}
-                    3. Tips: {'\n'}{'\n'}- Concentration and memory skills are crucial. Pay attention to the symbols on each card and try to remember their positions for future turns.
-                  </Text>
-                  <Text style={styles.heading}>Hangman Game Instructions</Text>
-                  <Text style={styles.text}>
-                    1. Gameplay:{'\n'}{'\n'}
-                    - Guess letters one at a time by suggesting a letter.{'\n'}
-                    - If the suggested letter is present in the word, reveal all occurrences of that letter in the word. {'\n'}
-                    - If the suggested letter is not in the word, add a body part to the hangman figure.{'\n'}
-                    - Continue guessing letters until the word is fully revealed or the hangman figure is complete.{'\n'}{'\n'}
-                    2. Winning: {'\n'}{'\n'}- The game is won if the player guesses all the letters in the word before the hangman figure is complete.{'\n'}
-                    - The game is lost if the hangman figure is complete before the word is fully revealed.{'\n'}{'\n'}
-                    3. Tips: {'\n'}{'\n'}-  Start with the most common letters in the English language (e.g., "E", "A", "R") and try to deduce the word based on the revealed letters and your knowledge of possible words.
-                  </Text>
-                </View>
-              </ScrollView>
-            </Modal>
           </View>
         </ImageBackground>
       </LinearGradient>
